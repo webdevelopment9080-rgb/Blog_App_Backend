ed16e75cd77c07f13cb564a6f4383bec98516341
webdevelopment9080-rgb <webdevelopment9080@gmail.com>
2026-06-06 16:47:55 +0500

    Add files via upload

--- config/db.config.cjs
+++ config/db.config.cjs
@@ -4,8 +4,9 @@ dotenv.config()
 
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, "", {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port:process.env.DB_PORT,
   dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 });
 
