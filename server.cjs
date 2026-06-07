1d229515ee2314a3d2472a2e29bdedb797d1f56e
webdevelopment9080-rgb <webdevelopment9080@gmail.com>
2026-06-07 14:06:55 +0500

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -5,7 +5,10 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 let port = 3000;
-
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USERNAME:", process.env.DB_USERNAME);
 
 
 (async ()=>{
